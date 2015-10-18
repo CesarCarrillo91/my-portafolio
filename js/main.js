e864e14593f578b2053dcf67e7052849054a4f9d
Cesar Carrillo <sistemas.carrillo01@gmail.com>
2015-10-18 13:57:13 -0500

    Removed all whitespaces (tab 2 consistency) from main.css and main.js

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ window.onload = function() {
 	// Taken from Udacity's Responsive Fundamentals Course
 	// https://www.udacity.com/course/viewer#!/c-ud893-nd/l-3561069759/m-3530719305
 	menu.addEventListener('click', function(e) {
-	  	navbar.classList.toggle('open');
-	  	e.stopPropagation();
+			navbar.classList.toggle('open');
+			e.stopPropagation();
 	});
 };
